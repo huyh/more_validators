fb89d50c13416ac9e76263952c4ebf1e74279d66
Huy Hoang <huythieuhoang@gmail.com>
2012-04-08 09:05:39 +1000

    Dont need namespace for validators

--- lib/more_validators/validators.rb
+++ lib/more_validators/validators.rb
@@ -1,27 +1,25 @@
 require 'active_model'
 require 'json'
 
-module MoreValidators
-  class JsonValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      unless valid_json?(value)
-        record.errors[attribute] << (options[:message] || 'must be in JSON format')
-      end
-    end
-
-    private
-    def valid_json?(json)
-      JSON.parse(json)
-    rescue
-      false
+class JsonValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless valid_json?(value)
+      record.errors[attribute] << (options[:message] || 'must be in JSON format')
     end
   end
 
-  class EmailValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-        record.errors[attribute] << (options[:message] || "is not a valid email") 
-      end
+  private  
+  def valid_json?(json)
+    JSON.parse(json)
+  rescue
+    false
+  end
+end  
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+      record.errors[attribute] << (options[:message] || "is not a valid email") 
     end
   end
 end
--- spec/more_validators/validators_spec.rb
+++ spec/more_validators/validators_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 require 'ostruct'
 
-
 describe "Validators" do
   let(:attribute) { :an_attr }
   let(:model) { OpenStruct.new(:errors => { attribute => [] }) }
   subject { model.errors[attribute] }
 
-  describe MoreValidators::JsonValidator do
-    let(:validator) { MoreValidators::JsonValidator.new({:attributes => attribute}) }
+  describe JsonValidator do
+    let(:validator) { JsonValidator.new({:attributes => attribute}) }
 
     it "returns if the value is a valid JSON string" do
       validator.validate_each(model, attribute, '{"first_name": "hello", "last_name": "world"}')
@@ -24,14 +23,14 @@ describe "Validators" do
     end
 
     it "can use a specified error message" do
-      validator = MoreValidators::JsonValidator.new({:attributes => attribute, :message => 'hello world error'}) 
+      validator = JsonValidator.new({:attributes => attribute, :message => 'hello world error'}) 
       validator.validate_each(model, attribute, 'hello')
       subject.should == ['hello world error']
     end
   end
 
-  describe MoreValidators::EmailValidator do
-    let(:validator) { MoreValidators::EmailValidator.new({:attributes => attribute}) }
+  describe EmailValidator do
+    let(:validator) { EmailValidator.new({:attributes => attribute}) }
 
     it "returns if the value is a valid email" do
       validator.validate_each(model, attribute, 'test@email.com')
