ae6f81dab00b419d8fd38b84dc9242c157d54264
Huy Hoang <huythieuhoang@gmail.com>
2012-04-07 23:15:50 +1000

    Add email validation

--- lib/more_validators/validators.rb
+++ lib/more_validators/validators.rb
@@ -16,4 +16,12 @@ module MoreValidators
       false
     end
   end
+
+  class EmailValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+        record.errors[attribute] << (options[:message] || "is not a valid email") 
+      end
+    end
+  end
 end
---
+++ spec/more_validators/validators_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+require 'ostruct'
+
+
+describe "Validators" do
+  let(:attribute) { :an_attr }
+  let(:model) { OpenStruct.new(:errors => { attribute => [] }) }
+  subject { model.errors[attribute] }
+
+  describe MoreValidators::JsonValidator do
+    let(:validator) { MoreValidators::JsonValidator.new({:attributes => attribute}) }
+
+    it "returns if the value is a valid JSON string" do
+      validator.validate_each(model, attribute, '{"first_name": "hello", "last_name": "world"}')
+      subject.should be_empty
+
+      validator.validate_each(model, attribute, 'hello')
+      subject.should_not be_empty
+    end
+
+    it "sets a default error message if there isn't one in the options" do
+      validator.validate_each(model, attribute, 'hello')
+      subject.should == ['must be in JSON format']
+    end
+
+    it "can use a specified error message" do
+      validator = MoreValidators::JsonValidator.new({:attributes => attribute, :message => 'hello world error'}) 
+      validator.validate_each(model, attribute, 'hello')
+      subject.should == ['hello world error']
+    end
+  end
+
+  describe MoreValidators::EmailValidator do
+    let(:validator) { MoreValidators::EmailValidator.new({:attributes => attribute}) }
+
+    it "returns if the value is a valid email" do
+      validator.validate_each(model, attribute, 'test@email.com')
+      subject.should be_empty
+
+      validator.validate_each(model, attribute, 'test@email')
+      subject.should_not be_empty
+    end
+  end
+end
